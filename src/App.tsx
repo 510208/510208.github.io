7815803037628508a4f6d54aea9c4a23a5ff707f
510208 <xux510208@gmail.com>
2025-07-29 12:50:48 +0800

    ✨ feat: 使用懶加載和懸浮效果優化路由加載性能

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,22 @@ import {
   Route,
   useLocation,
 } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Friends } from "./pages/Friends";
-import { Projects } from "./pages/Projects";
+// import { Home } from "./pages/Home";
+// import { Friends } from "./pages/Friends";
+// import { Projects } from "./pages/Projects";
 import { ReactLenis } from "lenis/react";
 import { useState, useEffect } from "react";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() =>
+  import("./pages/Home").then((module) => ({ default: module.Home }))
+);
+const Friends = lazy(() =>
+  import("./pages/Friends").then((module) => ({ default: module.Friends }))
+);
+const Projects = lazy(() =>
+  import("./pages/Projects").then((module) => ({ default: module.Projects }))
+);
 
 function AnimatedRoutes() {
   const location = useLocation();
@@ -32,11 +43,13 @@ function AnimatedRoutes() {
         }
       }}
     >
-      <Routes location={displayLocation}>
-        <Route path="/" element={<Home />} />
-        <Route path="/friends" element={<Friends />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+      <Suspense fallback={<div>燒等一下下...</div>}>
+        <Routes location={displayLocation}>
+          <Route path="/" element={<Home />} />
+          <Route path="/friends" element={<Friends />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
