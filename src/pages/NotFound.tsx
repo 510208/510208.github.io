7e1419a854dc5a372fab05eeab8744d96373c9e4
510208 <xux510208@gmail.com>
2025-07-30 17:36:08 +0800

    ✨ feat: 新增404頁面轉址到首頁以確保SPA的路由，並在路由中整合NotFound組件

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+export const NotFound = () => {
+  return (
+    <div className="bg-white dark:bg-black text-black dark:text-white">
+      <h1>404 - Not Found</h1>
+      <p>抱歉，您所尋找的頁面不存在。</p>
+    </div>
+  );
+};
