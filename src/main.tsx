0b9647834ec63043773ffed6c01443693c3201de
510208 <xux510208@gmail.com>
2025-08-21 12:38:28 +0800

    ✨ feat: 新增 CursorFollower 組件以實現自訂游標效果，並在主應用中整合使用

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import CursorFollower from "./components/CursorFollower";
 // Supports weights 100-900
 import "@fontsource-variable/noto-sans-tc";
 // Supports weights 100-900
@@ -24,6 +25,7 @@ TagManager.initialize(tagManagerArgs);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
+      <CursorFollower />
       <App />
     </ThemeProvider>
   </StrictMode>
