ae4dcefcc06fd3d6437833d8c8d75af7726b2bb9
510208 <xux510208@gmail.com>
2025-08-25 18:40:06 +0800

    ✨ feat: 整合導航菜單項目，新增工具提示功能並引入 OpenCommandButton

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,18 @@ import { Gravatar } from "./ui/Gravatar";
 import { ModeToggle } from "./ui/mode-toggle";
 // import Headroom from "react-headroom";
 import { useState, useEffect } from "react";
-import { UsersRound, FolderGit, Newspaper, ToolCase, Info } from "lucide-react";
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import OpenCommandButton from "./command/OpenCommandButton";
+import menuItems from "@/pages/PageList";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const Navbar = () => {
   const [isSticky, setIsSticky] = useState(false);
@@ -24,36 +30,8 @@ export const Navbar = () => {
     };
   }, []);
 
-  const menuItems = [
-    {
-      label: "ABOUT",
-      to: "/about",
-      icon: <Info size={18} />,
-    },
-    {
-      label: "FRIENDS",
-      to: "/friends",
-      icon: <UsersRound size={18} />,
-    },
-    {
-      label: "PROJECTS",
-      to: "/projects",
-      icon: <FolderGit size={18} />,
-    },
-    {
-      label: "EQUIPMENTS",
-      to: "/equipments",
-      icon: <ToolCase size={18} />,
-    },
-    {
-      label: "BLOG",
-      to: "/blog-posts",
-      icon: <Newspaper size={18} />,
-    },
-  ];
-
   return (
-    <div className="inset-x-0 top-0 lg:top-4 mx-auto sticky z-999 w-full lg:w-[calc(100%-theme(space.4))] max-w-7xl justify-center lg:border rounded-none lg:rounded-2xl overflow-hidden lg:shadow">
+    <div className="inset-x-0 top-0 lg:top-4 mx-auto sticky z-50 w-full lg:w-[calc(100%-theme(space.4))] max-w-7xl justify-center lg:border rounded-none lg:rounded-2xl overflow-hidden lg:shadow">
       <nav
         id="navbar"
         className={`border-none backdrop-blur-md ${
@@ -76,7 +54,7 @@ export const Navbar = () => {
 
             {/* 導航選單 - 右側，在手機版隱藏 */}
             <NavigationMenu className="hidden lg:flex items-center">
-              <NavigationMenuList className="flex items-center space-x-7">
+              <NavigationMenuList className="flex items-center space-x-7 mr-4">
                 {menuItems.map((item) => {
                   // 檢查item是否為目前頁面
                   const isActive = window.location.pathname === item.to;
@@ -84,18 +62,25 @@ export const Navbar = () => {
                   return (
                     <div key={item.label} className="flex items-center mr-0">
                       <NavigationMenuItem>
-                        <Link
-                          to={item.to}
-                          className={`text-sm font-medium font-noto uppercase transition-colors px-4 py-2 rounded-md ml-2 mr-2 h-[64px] ${
-                            item.label === "BLOG"
-                              ? "hover:text-neutral-50 dark:hover:text-neutral-900 hover:bg-gray-800 dark:hover:bg-neutral-100"
-                              : "hover:text-neutral-600 dark:hover:text-neutral-500 text-black dark:text-white"
-                          } ${
-                            isActive ? "bg-nav-link-indicator" : ""
-                          } transition-all duration-300`}
-                        >
-                          {item.label}
-                        </Link>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Link
+                              to={item.to}
+                              className={`text-sm font-medium font-noto uppercase transition-colors px-4 py-2 rounded-md ml-2 mr-2 h-[64px] ${
+                                item.label === "BLOG"
+                                  ? "hover:text-neutral-50 dark:hover:text-neutral-900 hover:bg-gray-800 dark:hover:bg-neutral-100"
+                                  : "hover:text-neutral-600 dark:hover:text-neutral-500 text-black dark:text-white"
+                              } ${
+                                isActive ? "bg-nav-link-indicator" : ""
+                              } transition-all duration-300`}
+                            >
+                              {item.label}
+                            </Link>
+                          </TooltipTrigger>
+                          <TooltipContent className="z-50">
+                            <span>{item.labelZh}</span>
+                          </TooltipContent>
+                        </Tooltip>
 
                         {/* 分隔符號 */}
                         {item.label !== "BLOG" && (
@@ -104,7 +89,7 @@ export const Navbar = () => {
                           </span>
                         )}
                         {item.label === "BLOG" && (
-                          <span className="text-neutral-400 text-sm font-noto mr-4">
+                          <span className="text-neutral-400 text-sm font-noto">
                             /
                           </span>
                         )}
@@ -113,6 +98,7 @@ export const Navbar = () => {
                   );
                 })}
               </NavigationMenuList>
+              <OpenCommandButton className="mr-4" />
               <ModeToggle />
             </NavigationMenu>
 
