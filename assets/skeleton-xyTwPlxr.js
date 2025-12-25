258fc5405ad21ba85450e8ecf471157b10c3a668
510208 <510208@users.noreply.github.com>
2025-12-25 01:10:09 +0000

    deploy: 3c3a01accc887495fd0da9c4408e2babd6ed2728

---
+++ assets/skeleton-xyTwPlxr.js
@@ -0,0 +1,1 @@
+import{j as o}from"./react-BQa3xukR.js";import{c as s}from"./index-CWWDAjPD.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
