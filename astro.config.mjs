0122c0062677c8c7b1c50e54dd49327466a1130c
SamHacker <xux510208@gmail.com>
2026-02-01 17:30:57 +0800

    ➕ feat(config): 新增 astro-lenis 整合至配置檔

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,12 @@ import react from "@astrojs/react";
 
 import devtoolBreakpoints from "astro-devtool-breakpoints";
 
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://510208.github.io",
-  integrations: [react(), devtoolBreakpoints()],
+  integrations: [react(), devtoolBreakpoints(), lenis()],
   vite: {
     plugins: [tailwindcss()],
   },
