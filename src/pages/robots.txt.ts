ba1aa48f4ece92556310723ea9234db278664383
SamHacker <xux510208@gmail.com>
2026-02-08 16:39:28 +0800

    ✨ feat(robots): 新增 robots.txt 以改善網站 SEO

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+
+const getRobotsTxt = (sitemapURL: URL) => `\
+User-agent: *
+Disallow: /
+Sitemap: ${sitemapURL.href}
+`;
+
+export const GET: APIRoute = ({ site }) => {
+  const sitemapURL = new URL("sitemap-index.xml", site);
+  return new Response(getRobotsTxt(sitemapURL));
+};
