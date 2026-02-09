222f869330977d8ed3c8bd5c73b830f0ef4cc2b6
SamHacker <xux510208@gmail.com>
2026-02-09 19:40:01 +0800

    💄 style(robots.txt): 更新 robots.txt 以改善搜尋引擎爬蟲的規則

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,7 +2,36 @@ import type { APIRoute } from "astro";
 
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
+Content-Signal: search=yes,ai-train=no
+Allow: /
+
+User-agent: archive.org_bot
+Allow: /
+
+User-agent: Amazonbot
 Disallow: /
+
+User-agent: Applebot-Extended
+Disallow: /
+
+User-agent: Bytespider
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+User-agent: ClaudeBot
+Disallow: /
+
+User-agent: Google-Extended
+Disallow: /
+
+User-agent: GPTBot
+Disallow: /
+
+User-agent: meta-externalagent
+Disallow: /
+
 Sitemap: ${sitemapURL.href}
 `;
 
