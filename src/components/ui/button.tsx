023348ed06181725f3d5a47283e6e168f8e8b9a3
SamHacker <xux510208@gmail.com>
2026-06-06 18:38:16 +0800

    💄 ui(button): 移除 icon-hero 按鈕變體的額外內邊距

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -29,7 +29,7 @@ const buttonVariants = cva(
         "icon-xs": "size-6 rounded-md [&_svg:not([class*='size-'])]:size-3",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
-        "icon-hero": "size-12 px-3",
+        "icon-hero": "size-12",
       },
     },
     defaultVariants: {
