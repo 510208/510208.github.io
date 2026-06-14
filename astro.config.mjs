6ec022ae114e4bfeec68ba555c675f7ab7e73055
SamHacker <xux510208@gmail.com>
2026-06-14 19:45:07 +0800

    ⚰️ fix(config): 移除不再使用的Partytown整合

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 import devtoolBreakpoints from "astro-devtool-breakpoints";
 import lenis from "astro-lenis";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import critters from 'astro-critters';
 
@@ -59,24 +58,7 @@ export default defineConfig({
     }
   ],
 
-  integrations: [react(), devtoolBreakpoints(), lenis(), partytown({
-    config: {
-      forward: ["dataLayer.push"],
-      resolveUrl: function (url, location, type) {
-        if (type === "script") {
-          const proxyUrl = new URL(url.href);
-          // 如果請求是 Google 相關，確保正確解析
-          if (
-            proxyUrl.hostname.includes("google-analytics.com") ||
-            proxyUrl.hostname.includes("googletagmanager.com")
-          ) {
-            return proxyUrl;
-          }
-        }
-        return url;
-      },
-    },
-  }), sitemap(), critters()],
+  integrations: [react(), devtoolBreakpoints(), lenis(), sitemap(), critters()],
 
   experimental: {
     rustCompiler: true
